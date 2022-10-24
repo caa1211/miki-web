f04f7fc8bda9338b9328523443385c9da76da6d1
Carter Chang <carter@vpn-pool-41-0-124.tw1.fy94.net>
2022-10-25 00:45:12 +0800

    add navibar

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,11 +1,15 @@
 import Head from 'next/head'
+import Navbar from '../navbar.js'
 import { Box, Container, Button, useColorMode } from '@chakra-ui/react'
 const Main = ({ children, router }) => {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
     <Box as="main" pb={8}>
-      <Head name="viewport" content="width=device-width, initial-scale=1" />
-      <title> Miki - Web </title>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title> Miki - Web </title>
+      </Head>
+      <Navbar path={router.asPath} />
       <Container maxW="container.md" pt={14}>
         <Button onClick={toggleColorMode}>
           Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
