3f468c1774a73307526ceb5a137e66617548bdc6
caa1211 <caa1211@users.noreply.github.com>
2026-08-30 14:48:03 +0800

    feat: add Google review showcase

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,6 +5,7 @@ export const siteConfig = {
   facebookUrl: 'https://www.facebook.com/profile.php?id=100054500738728',
   instagramUrl: 'https://www.instagram.com/45mikami/',
   xUrl: 'https://x.com/mikami_waxing',
+  googleReviewsUrl: 'https://www.google.com/maps/place/Mikami+Studio%E5%A5%B3%E6%80%A7%E5%B0%88%E5%B1%AC%E7%A7%81%E5%AF%86%E7%BE%8E%E8%82%8C%E5%B0%88%E9%96%80I%E7%BE%8E%E5%9C%8B%E9%A4%8A%E8%89%B2I%E7%84%A1%E7%97%9B%E7%84%A1%E5%89%B5%E6%B1%9D%E6%9A%88%E7%B2%89%E5%AB%A9I%E4%BD%8E%E6%BA%AB%E4%BD%8E%E7%97%9BVIO%E7%A7%81%E5%AF%86%E7%86%B1%E8%A0%9FI%E5%A5%B3%E6%80%A7%E7%A7%81%E5%AF%86%E7%86%B1%E8%A0%9FI%E5%8F%B0%E7%81%A3%E8%A1%9B%E7%A6%8F%E9%83%A8%E5%90%88%E6%A0%BC%E7%99%BB%E9%8C%84%E7%BE%8E%E5%9C%8BFDA%E8%AA%8D%E8%AD%89%E7%B2%89%E5%AB%A9%E8%A1%93/@24.8070592,120.9781356,17z/data=!4m8!3m7!1s0x3468354b1e555fdb:0x492dc2d664e8d5f!8m2!3d24.8070592!4d120.9781356!9m1!1b1!16s%2Fg%2F11hf1_lx95?entry=ttu',
   threadsUrl: '',
   phone: '+886919103591',
   phoneDisplay: '0919 103 591',
